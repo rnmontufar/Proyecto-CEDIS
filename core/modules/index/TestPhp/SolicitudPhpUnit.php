70855d72e7418a21d06d134e0ed8d3d5de62f860
Walter Danilo Zuniga Herrera <wzuniga@episunsa.edu.pe>
2016-12-21 01:57:37 -0500

    Adding More

--- core/modules/index/TestPhp/SolicitudPhpUnit.php
+++ core/modules/index/TestPhp/SolicitudPhpUnit.php
@@ -4,10 +4,49 @@
     include("../model/SolicitudData.php");
     class SolicitudPhpUnit extends PHPUnit_Framework_TestCase{
 
+        // Funcion para testing de la funcion getAll()
         public function testGetAll(){
-            $SolicitudData = new SolicitudData();
             
+            $SolicitudData = new SolicitudData();
+
+            $expected = ;
+
+            $this->assertEquals($expected, $SolicitudData->getAll());
+
         }
 
+
+        public function testUpdate(){
+            
+            $SolicitudData = new SolicitudData();
+
+            $expected = ;
+
+            $this->assertEquals($expected, $SolicitudData->update());
+
+        }
+
+
+        public function testGetById(){
+            
+            $SolicitudData = new SolicitudData();
+
+            $id = '1';
+
+            $expected = ;
+
+            $this->assertEquals($expected, $SolicitudData->getById($id));
+        }
+
+        
+        public function testGetEvery(){
+            
+            $SolicitudData = new SolicitudData();
+
+            $expected = ;
+
+            $this->assertEquals($expected, $SolicitudData->getEvery());
+
+        }
     }
 ?>
