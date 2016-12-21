0c22d1e3ac5d46733303d21210cdbb10c3ed5deb
Walter Danilo Zuniga Herrera <wzuniga@episunsa.edu.pe>
2016-12-21 01:34:58 -0500

    Adding com

--- core/modules/index/TestPhp/SolicitudTest.php
+++ core/modules/index/TestPhp/SolicitudTest.php
@@ -1,11 +1,29 @@
 <?php
 
+    //Configuracion de Assert BAIL, el programa terminara de ejecutarse
+    //si se produce error.
 	assert_options(ASSERT_BAIL, 1);
 
-    $id = '1';
 
+    //Test de recuperacion total de base de datos ordenados a apartir
+    //del orden de creacion.
     assert("getAll()","Assertion failed");
+
+
+    //Test de actualizacion de registro.
     assert("update()","Assertion failed");
+
+
+    //Test obtener un registro mediante ID.
+
+    //Test 1
+    $id = '1';
     assert("getById($id)","Assertion failed");
+
+    //Test 2
+    $id = '0';
+    assert("getById($id)","Assertion failed");
+
+    //
     assert("getEvery()","Assertion failed");
 ?>
